0c2516fcb2fc3db7a611412b807ef199766b623f
Tricia Sawyer <tricia.sawyer03@gmail>
2023-08-30 14:54:57 -0500

    chance movie search name

--- movieAPI.js
+++ movieAPI.js
@@ -4,8 +4,8 @@ const axios = require('axios');
 
 // Movie route
 async function getMovies(request, response) {
-    let cityMovieSearch = request.query.searchQuery;
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${cityMovieSearch}&page=1&include_adult=false`;
+    let movieSearch = request.query.searchQuery;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${ovieSearch}&page=1&include_adult=false`;
     
     try {
         const movieSearch = await axios.get(url);
